a7d25be98cfaae6d8068f4d1029278dc2c6f1334
Tomaz Bratanic <bratanic.tomaz@gmail.com>
2023-09-22 14:23:41 +0200

    streaming

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
 import os
+from typing import List, Any
 
 import streamlit as st
+from langchain.callbacks.base import BaseCallbackHandler
 from langchain.vectorstores.neo4j_vector import Neo4jVector
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.embeddings import OllamaEmbeddings, SentenceTransformerEmbeddings
 from langchain.chat_models import ChatOpenAI, ChatOllama
-from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.chains import ConversationalRetrievalChain
 from langchain.prompts.chat import (
     ChatPromptTemplate,
     SystemMessagePromptTemplate,
@@ -24,6 +26,17 @@ llm_name = os.getenv("LLM")
 
 os.environ["NEO4J_URL"] = url
 
+
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text=""):
+        self.container = container
+        self.text = initial_text
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        self.container.markdown(self.text)
+
+
 if embedding_model_name == "ollama":
     embeddings = OllamaEmbeddings(base_url=ollama_base_url, model="llama2")
     print("Embedding: Using Ollama")
@@ -33,15 +46,17 @@ elif embedding_model_name == "openai":
 else:
     embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
     print("Embedding: Using SentenceTransformer")
-    
+
 if llm_name == "gpt-4":
-    llm = ChatOpenAI(temperature=0, model_name="gpt-4")
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4", streaming=True)
     print("LLM: Using GPT-4")
 elif llm_name == "ollama":
-    llm = ChatOllama(temperature=0, base_url=ollama_base_url, model="llama2")
+    llm = ChatOllama(
+        temperature=0, base_url=ollama_base_url, model="llama2", streaming=True
+    )
     print("LLM: Using Ollama (llama2)")
 else:
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", streaming=True)
     print("LLM: Using GPT-3.5 Turbo")
 
 # LLM only response
@@ -54,13 +69,14 @@ chat_prompt = ChatPromptTemplate.from_messages(
 )
 
 
-def generate_llm_output(user_input: str) -> str:
+def generate_llm_output(user_input: str, callbacks: List[Any]) -> str:
     answer = llm(
         chat_prompt.format_prompt(
             text=user_input,
-        ).to_messages()
+        ).to_messages(),
+        callbacks=callbacks,
     ).content
-    return {"answer":answer,"sources":""}
+    return answer
 
 
 # Rag response
@@ -78,8 +94,8 @@ neo4j_db = Neo4jVector.from_existing_index(
 """,
 )
 
-qa = RetrievalQAWithSourcesChain.from_chain_type(
-    llm, chain_type="stuff", retriever=neo4j_db.as_retriever(search_kwargs={"k": 2})
+qa = ConversationalRetrievalChain.from_llm(
+    llm, retriever=neo4j_db.as_retriever(search_kwargs={"k": 2})
 )
 
 # Rag + KG
@@ -103,8 +119,8 @@ RETURN node.body + '\n' + answerTexts AS text, score, {source:node.link} AS meta
 """,
 )
 
-kg_qa = RetrievalQAWithSourcesChain.from_chain_type(
-    llm, chain_type="stuff", retriever=kg.as_retriever(search_kwargs={"k": 2})
+kg_qa = ConversationalRetrievalChain.from_llm(
+    llm, retriever=kg.as_retriever(search_kwargs={"k": 2})
 )
 
 # Streamlit stuff
@@ -123,34 +139,38 @@ st.markdown(styl, unsafe_allow_html=True)
 
 
 def chat_input():
+    user_input = st.chat_input("What coding issue can I help you resolve today?")
+
+    if user_input:
+        with st.chat_message("user"):
+            st.write(user_input)
+        with st.chat_message("assistant"):
+            st.caption(name)
+            stream_handler = StreamHandler(st.empty())
+            result = output_function(
+                {"question": user_input, "chat_history": []}, callbacks=[stream_handler]
+            )
+            output = result  # ["answer"] + "\n" + result["sources"]
+            st.session_state[f"user_input"].append(user_input)
+            st.session_state[f"generated"].append(output)
+            st.session_state[f"rag_mode"].append(name)
+
+
+def display_chat():
     # Session state
     if "generated" not in st.session_state:
         st.session_state[f"generated"] = []
 
     if "user_input" not in st.session_state:
+
         st.session_state[f"user_input"] = []
 
     if "rag_mode" not in st.session_state:
         st.session_state[f"rag_mode"] = []
-
-    user_input = st.chat_input("What coding issue can I help you resolve today?")
-
-    if user_input:
-        try:
-            result = output_function(user_input)
-            if not type(result) is list: result=[result]
-            output = "\n".join([data["answer"] + "\n" + data["sources"] for data in result])
-        except (KeyError, TypeError): # todo should we really have control-flow by exception?
-            output = output_function(user_input)
-        st.session_state[f"user_input"].append(user_input)
-        st.session_state[f"generated"].append(output)
-        st.session_state[f"rag_mode"].append(name)
-
-
-def display_chat():
     if st.session_state[f"generated"]:
         size = len(st.session_state[f"generated"])
         # Display only the last three exchanges
+        # Excluding the latest since it's streamed
         for i in range(max(size - 3, 0), size):
             with st.chat_message("user"):
                 st.write(st.session_state[f"user_input"][i])
@@ -169,9 +189,9 @@ name = mode_select()
 if name == "LLM only":
     output_function = generate_llm_output
 elif name == "Vector":
-    output_function = qa
+    output_function = qa.run
 elif name == "Vector + Graph":
-    output_function = kg_qa
+    output_function = kg_qa.run
 
-chat_input()
 display_chat()
+chat_input()
