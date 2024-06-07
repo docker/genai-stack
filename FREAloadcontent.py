59fdcc2b53f39d7e0e3091fc2cdbed6ee84a164c
threelaws010 <threelaws010@gmail.com>
2024-06-07 12:02:40 -0400

    adding load function

---
+++ FREAloadcontent.py
@@ -0,0 +1,111 @@
+#'text/html': 
+def funcWebPages():
+    pass
+
+ #       'text/markdown': 
+def funcMarkdown():
+    pass
+
+ #       'application/xml': 
+def funcXML():
+    pass
+
+ #       'application/pdf': 
+def funcPDF():
+    pass
+
+ #       'application/msword': 
+def funcDOC():
+    pass
+
+ #       'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 
+def funcDOCX():
+    pass
+
+  #      'application/vnd.ms-excel (XLS)':
+def funcXLS():
+    pass
+
+ #       'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+def funcXLSX():
+    pass
+
+ #       'application/vnd.ms-powerpoint (PPT)':
+def funcPPT():
+    pass
+
+ #       'application/vnd.openxmlformats-officedocument.presentationml.presentation':
+def funcPPTX():
+    pass
+
+ #       'application/rtf':
+def funcRTF():
+    pass
+
+ #       'image/jpeg':
+def funcJPG():
+    pass
+
+  #      'image/png':
+def funcPNG():
+    pass
+
+ #       'image/gif': 
+def funcGIF():
+    pass
+
+#        'image/bmp': 
+def funcBMP():
+    pass
+
+ #       'image/tiff':
+def funcTIFF():
+    pass
+
+   #     'application/javascript': 
+def funcJavaScript():
+    pass
+
+  #      'application/zip': 
+def funcZIP():
+    pass
+
+  #      'application/gzip': 
+def funcGZIP():
+    pass
+
+  #      'audio/mpeg': 
+def funcMP3():
+    pass
+
+#        'video/mp4': 
+def funcMP4():
+    pass
+
+    #    'audio/wav': 
+def funcWAV():
+    pass
+
+ #       'audio/ogg': 
+def funcOGG():
+    pass
+
+  #      'video/webm': 
+def funcWEBM():
+    pass
+
+ #       'application/json': 
+def funcJSON():
+    pass
+
+ #       'application/x-yaml': 
+def funcYAML():
+    pass
+
+ #       'application/epub+zip': 
+def funcEPUB():
+    pass
+
+ #       'application/x-mobipocket-ebook': 
+def funcMOBI():
+    pass
--- FREAloader.py
+++ FREAloader.py
@@ -8,6 +8,7 @@ from streamlit.logger import get_logger
 from chains import load_embedding_model
 from utils import create_constraints, create_vector_index
 from PIL import Image
+import FREAloadcontent as FREAloadcontent
 
 
 load_dotenv(".env")
@@ -59,34 +60,34 @@ def get_file_info(file_path):
 
 def switch_case(value):
     switch = {
-        'text/html': funcWebPages,
-        'text/markdown': funcMarkdown,
-        'application/xml': funcXML,
-        'application/pdf': funcPDF,
-        'application/msword': funcDOC,
-        'application/vnd.openxmlformats-officedocument.wordprocessingml.document': funcDOCX,
-        'application/vnd.ms-excel (XLS)': funcXLS,
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': funcXLSX,
-        'application/vnd.ms-powerpoint (PPT)': funcPPT,
-        'application/vnd.openxmlformats-officedocument.presentationml.presentation': funcPPTX,
-        'application/rtf': funcRTF,
-        'image/jpeg': funcJPG,
-        'image/png': funcPNG,
-        'image/gif': funcGIF,
-        'image/bmp': funcBMP,
-        'image/tiff': funcTIFF,
-        'application/javascript': funcJavaScript,
-        'application/zip': funcZIP,
-        'application/gzip': funcGZIP,
-        'audio/mpeg': funcMP3,
-        'video/mp4': funcMP4,
-        'audio/wav': funcWAV,
-        'audio/ogg': funcOGG,
-        'video/webm': funcWEBM,
-        'application/json': funcJSON,
-        'application/x-yaml': funcYAML,
-        'application/epub+zip': funcEPUB,
-        'application/x-mobipocket-ebook': funcMOBI
+        'text/html': FREAloadcontent.funcWebPages,
+        'text/markdown': FREAloadcontent.funcMarkdown,
+        'application/xml': FREAloadcontent.funcXML,
+        'application/pdf': FREAloadcontent.funcPDF,
+        'application/msword': FREAloadcontent.funcDOC,
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document': FREAloadcontent.funcDOCX,
+        'application/vnd.ms-excel (XLS)': FREAloadcontent.funcXLS,
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': FREAloadcontent.funcXLSX,
+        'application/vnd.ms-powerpoint (PPT)': FREAloadcontent.funcPPT,
+        'application/vnd.openxmlformats-officedocument.presentationml.presentation': FREAloadcontent.funcPPTX,
+        'application/rtf': FREAloadcontent.funcRTF,
+        'image/jpeg': FREAloadcontent.funcJPG,
+        'image/png': FREAloadcontent.funcPNG,
+        'image/gif': FREAloadcontent.funcGIF,
+        'image/bmp': FREAloadcontent.funcBMP,
+        'image/tiff': FREAloadcontent.funcTIFF,
+        'application/javascript': FREAloadcontent.funcJavaScript,
+        'application/zip': FREAloadcontent.funcZIP,
+        'application/gzip': FREAloadcontent.funcGZIP,
+        'audio/mpeg': FREAloadcontent.funcMP3,
+        'video/mp4': FREAloadcontent.funcMP4,
+        'audio/wav': FREAloadcontent.funcWAV,
+        'audio/ogg': FREAloadcontent.funcOGG,
+        'video/webm': FREAloadcontent.funcWEBM,
+        'application/json': FREAloadcontent.funcJSON,
+        'application/x-yaml': FREAloadcontent.funcYAML,
+        'application/epub+zip': FREAloadcontent.funcEPUB,
+        'application/x-mobipocket-ebook': FREAloadcontent.funcMOBI
     }
     func = switch.get(value, default_function)
     func()
