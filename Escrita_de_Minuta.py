85e7def3a9de340085b7e88243589f768517ad07
Leonardo Valeriano Neri <leonardovalerianoneri@gmail.com>
2025-06-17 15:37:03 -0300

    Adding page for new feature. Organizing home page to route multiple pages for 2 app features.

---
+++ Escrita_de_Minuta.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title(body='✍️ StartLegal - Escritor de Minutas')
+st.header("Assistente de Elaboração de Escrituras", divider='gray', )
+
+st.write(
+    "Anexe os documentos necessários das partes compradora e vendedora e a escritura do imóvel."
+)
